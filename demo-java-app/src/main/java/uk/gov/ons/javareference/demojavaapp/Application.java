1850a88487bca8697c0e556c4983742b4ed6cda9
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2021-04-30 08:54:00 +0100

    changed schema to cases

--- demo-java-app/src/main/java/uk/gov/ons/javareference/demojavaapp/Application.java
+++ demo-java-app/src/main/java/uk/gov/ons/javareference/demojavaapp/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoJavaAppApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoJavaAppApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
